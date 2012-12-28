07e3bf0b8533da4be8e314e49041b8407d48c6e9
deepak <de_best4u@yahoo.co.in>
2012-12-28 13:32:04 +0530

    add changes

---
+++ ShoppingCart.WebSite/Areas/Customer/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models= ShoppingCart.WebSite.Models;
+
+namespace ShoppingCart.WebSite.Areas.Customer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/Customer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Add(Models.Customer obj)
+        {
+           // return View();
+            return Json("");
+        }
+
+    }
+}
---
+++ ShoppingCart.WebSite/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.WebSite.Models
+{
+    public class Customer
+    {
+        public string productName { get; set; }
+        public string quantity { get; set; }
+    }
+}
